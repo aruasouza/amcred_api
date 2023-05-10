5e0080ef1155a521f0e097e027c8fcebbfd6a4d8
aruasouza <arua.viso@gmail.com>
2023-05-10 18:03:05 -0300

    normal

--- app.py
+++ app.py
@@ -11,5 +11,5 @@ def create_app():
 app = create_app()
 
 if __name__ == '__main__':
-    # serve(app,host='0.0.0.0', port=5000)
-    app.run(host='0.0.0.0', port=5000, debug = False)
+    serve(app,host='0.0.0.0', port=5000)
+    # app.run(host='0.0.0.0', port=5000, debug = False)
