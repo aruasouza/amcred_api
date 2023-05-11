d6d526800ee520c3ece690f6c88f4cdc9fc3f618
aruasouza <arua.viso@gmail.com>
2023-05-11 15:43:54 -0300

    better_model

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import requests
 
-# url = 'https://api-amcred.onrender.com'
-url = 'http://127.0.0.1:5000'
+url = 'https://api-amcred.onrender.com'
+# url = 'http://127.0.0.1:5000'
 secret = open('admin_pass','r').read()
 
-req = requests.get(url,auth = ('deepen',secret),json = {})
+req = requests.get(url,auth = ('deepen',secret),json = {'situacaodoimovelderesidencia':'Cedida',
+                                                        'situacaoimoveldeatividade':'Alugada',
+                                                        'tipodeponto':'Fixo',
+                                                        'formacaoescolar':'Sem Instrução',
+                                                        'constituicao':'M.E.I.'})
 print(req)
 print(req.json())
