5a34363f908a528a901252de95bdaee9f85230ee
aruasouza <arua.viso@gmail.com>
2023-06-02 16:36:30 -0300

    new_model

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 
-# url = 'https://api-amcred.onrender.com'
-url = 'http://127.0.0.1:5000'
+url = 'https://api-amcred.onrender.com'
+# url = 'http://127.0.0.1:5000'
 secret = open('admin_pass','r').read()
 
 req = requests.post(url,auth = ('deepen',secret),json = {'situacaodoimovelderesidencia':'Cedida',
